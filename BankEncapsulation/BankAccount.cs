744a3f0e234bdebcf918f54822e16e5677492518
Jordan Brown <j.d.brown329@gmail.com>
2022-10-25 09:48:13 -0500

    Finished

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+
+        private double _balance = 0;
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;        }
+
+       
+     
+    
+    
+
+    }
+}
+    
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BankEncapsulation;
+class program 
+{
+    static void Main(string[] args)
+    {
+        var b1 = new BankAccount();
+        b1.Deposit(500);
+
+        var bal = b1.GetBalance();
+        
+        Console.WriteLine($"{bal}");
+
+        b1.Withdraw(220);
+      
+     }
+}
+
+
+
